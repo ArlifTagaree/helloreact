4e25217bd5a12f5d63abaf2ee94f6bc114d7834b
ArlifTagaree <liffism9@gmail.com>
2023-04-03 16:02:51 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const age = 16;
+
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
+    <> {/* <> its mean root*/}
+    
+        <ul>
+          <li>Applesauce</li>
+          { age > 15 && <li>Brussels Sprouts</li> }
+          { age > 20 && <li>Oysters</li> }
+          { age > 25 && <li>Grappa</li> }
+        </ul>
     </>
   );
 }
